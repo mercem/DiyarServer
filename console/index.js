f2b133e5b83e5d40b34a95c0f641e694f3bf0b06
Omer Cem Akyol <omercema@gmail.com>
2019-03-16 16:47:56 +0300

    console added to server

---
+++ console/index.js
@@ -0,0 +1,14 @@
+var repl = require('repl');
+
+var {User, Roles} = require('../app/components/users/model');
+var {Model} = require('../app/components/models/model');
+
+var envName = process.env.NODE_ENV || "dev";
+var replServer = repl.start({
+  prompt: "Diyar(" + envName + ")>",
+});
+
+replServer.context.User = User;
+replServer.context.Model = Model;
+
+console.log("Console is up and ready to roll!");
--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-require('./configs/config');
+require('./configs');
 const express = require('express');
 const path = require('path');
 
 const app = express();
-const port = process.env.PORT; // Gets PORT from heroku
+const port = process.env.PORT;
 const routes = require('./app/routes');
 
 app.use(express.json());
@@ -11,7 +11,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/v0', routes);
 
-app.listen(port, () => {console.log(`Server is up at port ${port}`);});
+app.listen(port, () => {console.log(`Server is up at port ${port}`)});
 
 module.exports = app;
-
