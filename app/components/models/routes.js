5a7598a86e04afb4ea324eca3d4abdd36cb5d91a
Omer Cem Akyol <omercema@gmail.com>
2019-03-26 20:09:05 +0300

    put request routed to post for model create

--- app/components/models/routes.js
+++ app/components/models/routes.js
@@ -3,6 +3,7 @@ const controller = require('./controller');
 const {authendicate} = require('../../middleware/auth');
 
 router.post('/', authendicate, controller.create);
+router.put('/', authendicate, controller.create);
 router.get('/', controller.all);
 router.get('/me', authendicate, controller.me);
 router.post('/update', authendicate, controller.update);
