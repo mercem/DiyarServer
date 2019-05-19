0ff699a00fc2e324313f771249cab40dd0fbd45d
Omer Cem Akyol <omercema@gmail.com>
2019-05-19 17:09:41 +0300

    config updated for heroku

--- config/index.js
+++ config/index.js
@@ -1,7 +1,8 @@
-require('dotenv')
-  .config(
-    { 
-      path: `config/${process.env.NODE_ENV}.env`
-    });
+if(process.env.NODE_ENV != 'production')
+  require('dotenv')
+    .config(
+      { 
+        path: `config/${process.env.NODE_ENV}.env`
+      });
 
 console.log('NODE_ENV:', process.env.NODE_ENV)
